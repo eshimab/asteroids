ab6f3831bc5cb39271eac1dfe96f7b346b99f3c3
Eric <eric21@hawaii.edu>
2024-10-01 13:36:32 -1000

    Commit 2.2 added began game loop

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pygame
-
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"checking variables from costants.py")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    print(f"initializing pygame")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 
 if __name__ == "__main__":
